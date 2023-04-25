a0b1b4b225f7ba4d5574f888d6928245e0347cf7
plastikfan <plastikfan@outlook.com>
2023-04-25 11:38:54 +0100

    fix(i18n): negotiate should add new localizers (#241)

--- i18n/translator_test.go
+++ i18n/translator_test.go
@@ -8,33 +8,16 @@ import (
 	. "github.com/onsi/gomega"
 	"golang.org/x/text/language"
 
-	xi18n "github.com/snivilised/extendio/i18n"
+	"github.com/snivilised/extendio/i18n"
 	"github.com/snivilised/extendio/internal/helpers"
 	"github.com/snivilised/extendio/xfs/utils"
 )
-
-// This is an example of how a client library should implement
-// their Use function. In this particular case, the addition
-// source is already present, so the result of AppendSources
-// will contain the source id of the duplicated dependency, ie
-// "test.graffico", which is simply ignored as it should be.
-func clientUse(options ...xi18n.UseOptionFn) error {
-	o := append(options, func(uo *xi18n.UseOptions) { //nolint:gocritic // no alternative known
-		_ = uo.From.AppendSources(&xi18n.TranslationFiles{
-			GrafficoSourceID: xi18n.TranslationSource{
-				Name: "test.graffico",
-			},
-		})
-	})
-
-	return xi18n.Use(o...)
-}
 
 var _ = Describe("Translator", Ordered, func() {
 	var (
 		repo                string
 		l10nPath            string
-		testTranslationFile xi18n.TranslationFiles
+		testTranslationFile i18n.TranslationFiles
 	)
 
 	BeforeAll(func() {
@@ -42,81 +25,21 @@ var _ = Describe("Translator", Ordered, func() {
 		l10nPath = helpers.Path(repo, "Test/data/l10n")
 		Expect(utils.FolderExists(l10nPath)).To(BeTrue())
 
-		testTranslationFile = xi18n.TranslationFiles{
-			xi18n.ExtendioSourceID: xi18n.TranslationSource{Name: "test"},
+		testTranslationFile = i18n.TranslationFiles{
+			i18n.ExtendioSourceID: i18n.TranslationSource{Name: "test"},
 		}
 	})
 
 	BeforeEach(func() {
-		xi18n.ResetTx()
+		i18n.ResetTx()
 	})
 
 	Context("xi18n.TxRef.IsNone", func() {
 		When("not Use'd", func() {
 			It("🧪 should: be true", func() {
-				Expect(xi18n.TxRef.IsNone()).To(BeTrue(),
+				Expect(i18n.TxRef.IsNone()).To(BeTrue(),
 					"'Use' not invoked but xi18n.TxRef.IsNone indicates, its still set?",
 				)
-			})
-		})
-	})
-
-	Context("LoadFrom", func() {
-		When("duplicate sources for the same dependency", func() {
-			It("🧪 should: return duplicate", func() {
-				from := xi18n.LoadFrom{
-					Path: l10nPath,
-					Sources: xi18n.TranslationFiles{
-						GrafficoSourceID: xi18n.TranslationSource{
-							Name: "test.graffico",
-						},
-					},
-				}
-				duplicates := from.AppendSources(&xi18n.TranslationFiles{
-					GrafficoSourceID: xi18n.TranslationSource{
-						Name: "test.graffico",
-					},
-				})
-				Expect(duplicates[0]).To(Equal("test.graffico"))
-			})
-		})
-
-		When("client Use function uses a dependency already registered", func() {
-			It("🧪 should: ignore the duplicate", func() {
-				err := clientUse(func(uo *xi18n.UseOptions) {
-					uo.From = xi18n.LoadFrom{
-						Path: l10nPath,
-						Sources: xi18n.TranslationFiles{
-							GrafficoSourceID: xi18n.TranslationSource{
-								Name: "test.graffico",
-							},
-						},
-					}
-				})
-				Expect(err).Error().To(BeNil())
-			})
-		})
-	})
-
-	Context("AppendSources", func() {
-		Context("given: Translator exists", func() {
-			It("🧪 should: be able to add source", func() {
-				if err := xi18n.Use(func(o *xi18n.UseOptions) {
-					o.Tag = language.AmericanEnglish
-					o.From.Path = l10nPath
-					o.From.Sources = testTranslationFile
-				}); err != nil {
-					Fail(err.Error())
-				}
-
-				additional := make(xi18n.TranslationFiles)
-				additional[GrafficoSourceID] = xi18n.TranslationSource{
-					Name: "test.graffico",
-				}
-
-				xi18n.TxRef.Get().LanguageInfoRef().Get().From.AppendSources(&additional)
-				_, found := xi18n.TxRef.Get().LanguageInfoRef().Get().From.Sources[GrafficoSourceID]
-				Expect(found).To(BeTrue())
 			})
 		})
 	})
@@ -124,27 +47,27 @@ var _ = Describe("Translator", Ordered, func() {
 	Context("Use", func() {
 		When("requested language is available", func() {
 			It("🧪 should: create Translator", func() {
-				if err := xi18n.Use(func(o *xi18n.UseOptions) {
+				if err := i18n.Use(func(o *i18n.UseOptions) {
 					o.Tag = language.AmericanEnglish
 					o.From.Path = l10nPath
 					o.From.Sources = testTranslationFile
 				}); err != nil {
 					Fail(err.Error())
 				}
-				Expect(xi18n.TxRef.IsNone()).To(BeFalse())
-				Expect(xi18n.TxRef.Get().LanguageInfoRef().Get().Tag).To(Equal(language.AmericanEnglish))
+				Expect(i18n.TxRef.IsNone()).To(BeFalse())
+				Expect(i18n.TxRef.Get().LanguageInfoRef().Get().Tag).To(Equal(language.AmericanEnglish))
 			})
 		})
 
 		When("requested language is the default", func() {
 			It("🧪 should: create Translator", func() {
-				if err := xi18n.Use(func(o *xi18n.UseOptions) {
+				if err := i18n.Use(func(o *i18n.UseOptions) {
 					o.Tag = language.BritishEnglish
 				}); err != nil {
 					Fail(err.Error())
 				}
-				Expect(xi18n.TxRef.IsNone()).To(BeFalse())
-				Expect(xi18n.TxRef.Get().LanguageInfoRef().Get().Tag).To(Equal(language.BritishEnglish))
+				Expect(i18n.TxRef.IsNone()).To(BeFalse())
+				Expect(i18n.TxRef.Get().LanguageInfoRef().Get().Tag).To(Equal(language.BritishEnglish))
 			})
 		})
 
@@ -152,7 +75,7 @@ var _ = Describe("Translator", Ordered, func() {
 			When("requested language is NOT available", func() {
 				It("🧪 should: return NOT error", func() {
 					requested := language.Spanish
-					if err := xi18n.Use(func(o *xi18n.UseOptions) {
+					if err := i18n.Use(func(o *i18n.UseOptions) {
 						o.Tag = requested
 					}); err != nil {
 						Fail(
@@ -170,7 +93,7 @@ var _ = Describe("Translator", Ordered, func() {
 			When("requested language is NOT available", func() {
 				It("🧪 should: return error", func() {
 					requested := language.Spanish
-					if err := xi18n.Use(func(o *xi18n.UseOptions) {
+					if err := i18n.Use(func(o *i18n.UseOptions) {
 						o.DefaultIsAcceptable = false
 						o.Tag = requested
 					}); err == nil {
@@ -187,56 +110,56 @@ var _ = Describe("Translator", Ordered, func() {
 		When("client provides Create function", func() {
 			It("🧪 should: create the localizer with the override", func() {
 				dummy := helpers.DummyCreator{}
-				if err := xi18n.Use(func(o *xi18n.UseOptions) {
+				if err := i18n.Use(func(o *i18n.UseOptions) {
 					o.Tag = language.BritishEnglish
 					o.Create = dummy.Create
 				}); err != nil {
 					Fail(err.Error())
 				}
 				Expect(dummy.Invoked).To(BeTrue())
-				Expect(xi18n.TxRef.IsNone()).To(BeFalse())
-				Expect(xi18n.TxRef.Get().LanguageInfoRef().Get().Tag).To(Equal(language.BritishEnglish))
+				Expect(i18n.TxRef.IsNone()).To(BeFalse())
+				Expect(i18n.TxRef.Get().LanguageInfoRef().Get().Tag).To(Equal(language.BritishEnglish))
 			})
 		})
 
 		When("extendio source not provided", func() {
 			It("🧪 should: create Translator", func() {
-				from := xi18n.LoadFrom{
+				from := i18n.LoadFrom{
 					Path: l10nPath,
-					Sources: xi18n.TranslationFiles{
-						GrafficoSourceID: xi18n.TranslationSource{Name: "test.graffico"},
+					Sources: i18n.TranslationFiles{
+						GrafficoSourceID: i18n.TranslationSource{Name: "test.graffico"},
 					},
 				}
 
-				if err := xi18n.Use(func(o *xi18n.UseOptions) {
+				if err := i18n.Use(func(o *i18n.UseOptions) {
 					o.Tag = language.BritishEnglish
 					o.From = from
 				}); err != nil {
 					Fail(err.Error())
 				}
-				Expect(xi18n.TxRef.IsNone()).To(BeFalse())
-				Expect(xi18n.TxRef.Get().LanguageInfoRef().Get().Tag).To(Equal(language.BritishEnglish))
+				Expect(i18n.TxRef.IsNone()).To(BeFalse())
+				Expect(i18n.TxRef.Get().LanguageInfoRef().Get().Tag).To(Equal(language.BritishEnglish))
 			})
 		})
 
-		When("use invoked multiple times", func() {
-			Context("given: new source already present", func() {
+		Context("negotiate translators", func() {
+			When("new source already present", func() {
 				It("🧪 should: ignore subsequent use source", func() {
-					if err := xi18n.Use(func(o *xi18n.UseOptions) {
+					if err := i18n.Use(func(o *i18n.UseOptions) {
 						o.Tag = language.AmericanEnglish
 						o.From.Path = l10nPath
-						o.From.Sources = xi18n.TranslationFiles{
-							xi18n.ExtendioSourceID: xi18n.TranslationSource{Name: "test"},
+						o.From.Sources = i18n.TranslationFiles{
+							i18n.ExtendioSourceID: i18n.TranslationSource{Name: "test"},
 						}
 					}); err != nil {
 						Fail(err.Error())
 					}
 
-					if err := xi18n.Use(func(o *xi18n.UseOptions) {
+					if err := i18n.Use(func(o *i18n.UseOptions) {
 						o.Tag = language.AmericanEnglish
 						o.From.Path = "/foo-bar"
-						o.From.Sources = xi18n.TranslationFiles{
-							xi18n.ExtendioSourceID: xi18n.TranslationSource{Name: "test"},
+						o.From.Sources = i18n.TranslationFiles{
+							i18n.ExtendioSourceID: i18n.TranslationSource{Name: "test"},
 						}
 					}); err != nil {
 						Fail(err.Error())
@@ -244,29 +167,29 @@ var _ = Describe("Translator", Ordered, func() {
 				})
 			})
 
-			Context("given: new source already NOT already present", func() {
+			When("new source already NOT already present", func() {
 				It("🧪 should: add new source", func() {
-					if err := xi18n.Use(func(o *xi18n.UseOptions) {
+					if err := i18n.Use(func(o *i18n.UseOptions) {
 						o.Tag = language.AmericanEnglish
 						o.From.Path = l10nPath
-						o.From.Sources = xi18n.TranslationFiles{
-							xi18n.ExtendioSourceID: xi18n.TranslationSource{Name: "test"},
+						o.From.Sources = i18n.TranslationFiles{
+							i18n.ExtendioSourceID: i18n.TranslationSource{Name: "test"},
 						}
 					}); err != nil {
 						Fail(err.Error())
 					}
 
-					if err := xi18n.Use(func(o *xi18n.UseOptions) {
+					if err := i18n.Use(func(o *i18n.UseOptions) {
 						o.Tag = language.AmericanEnglish
 						o.From.Path = l10nPath
-						o.From.Sources = xi18n.TranslationFiles{
-							GrafficoSourceID: xi18n.TranslationSource{Name: "test.graffico"},
+						o.From.Sources = i18n.TranslationFiles{
+							GrafficoSourceID: i18n.TranslationSource{Name: "test.graffico"},
 						}
 					}); err != nil {
 						Fail(err.Error())
 					}
 
-					actual := xi18n.Text(PavementGraffitiReportTemplData{
+					actual := i18n.Text(PavementGraffitiReportTemplData{
 						Primary: "Violet",
 					})
 					Expect(actual).To(Equal(expectUS))
@@ -279,13 +202,13 @@ var _ = Describe("Translator", Ordered, func() {
 		Context("given: default is acceptable", func() {
 			When("invalid translation source specified", func() {
 				It("🧪 should: NOT return error", func() {
-					if err := xi18n.Use(func(o *xi18n.UseOptions) {
+					if err := i18n.Use(func(o *i18n.UseOptions) {
 						o.Tag = language.EuropeanSpanish
-						o.From = xi18n.LoadFrom{
+						o.From = i18n.LoadFrom{
 							Path: l10nPath,
-							Sources: xi18n.TranslationFiles{
-								xi18n.ExtendioSourceID: xi18n.TranslationSource{Name: "scooby-doo"},
-								GrafficoSourceID:       xi18n.TranslationSource{Name: "test.graffico"},
+							Sources: i18n.TranslationFiles{
+								i18n.ExtendioSourceID: i18n.TranslationSource{Name: "scooby-doo"},
+								GrafficoSourceID:      i18n.TranslationSource{Name: "test.graffico"},
 							},
 						}
 					}); err != nil {
@@ -306,12 +229,12 @@ var _ = Describe("Translator", Ordered, func() {
 							}
 						}()
 
-						_ = xi18n.Use(func(o *xi18n.UseOptions) {
+						_ = i18n.Use(func(o *i18n.UseOptions) {
 							o.DefaultIsAcceptable = false
 							o.Tag = language.AmericanEnglish
-							o.From = xi18n.LoadFrom{
-								Sources: xi18n.TranslationFiles{
-									GrafficoSourceID: xi18n.TranslationSource{Name: "test.graffico"},
+							o.From = i18n.LoadFrom{
+								Sources: i18n.TranslationFiles{
+									GrafficoSourceID: i18n.TranslationSource{Name: "test.graffico"},
 								},
 							}
 						})
@@ -336,14 +259,14 @@ var _ = Describe("Translator", Ordered, func() {
 								)
 							}
 						}()
-						if err := xi18n.Use(func(o *xi18n.UseOptions) {
+						if err := i18n.Use(func(o *i18n.UseOptions) {
 							o.DefaultIsAcceptable = false
 							o.Tag = language.AmericanEnglish
-							o.From = xi18n.LoadFrom{
+							o.From = i18n.LoadFrom{
 								Path: l10nPath,
-								Sources: xi18n.TranslationFiles{
-									xi18n.ExtendioSourceID: xi18n.TranslationSource{Name: "scooby-doo"},
-									GrafficoSourceID:       xi18n.TranslationSource{Name: "test.graffico"},
+								Sources: i18n.TranslationFiles{
+									i18n.ExtendioSourceID: i18n.TranslationSource{Name: "scooby-doo"},
+									GrafficoSourceID:      i18n.TranslationSource{Name: "test.graffico"},
 								},
 							}
 						}); err == nil {
@@ -356,14 +279,14 @@ var _ = Describe("Translator", Ordered, func() {
 			Context("requested NOT supported", func() {
 				When("invalid translation source specified", func() {
 					It("🧪 should: return error", func() {
-						if err := xi18n.Use(func(o *xi18n.UseOptions) {
+						if err := i18n.Use(func(o *i18n.UseOptions) {
 							o.DefaultIsAcceptable = false
 							o.Tag = language.EuropeanSpanish
-							o.From = xi18n.LoadFrom{
+							o.From = i18n.LoadFrom{
 								Path: l10nPath,
-								Sources: xi18n.TranslationFiles{
-									xi18n.ExtendioSourceID: xi18n.TranslationSource{Name: "scooby-doo"},
-									GrafficoSourceID:       xi18n.TranslationSource{Name: "test.graffico"},
+								Sources: i18n.TranslationFiles{
+									i18n.ExtendioSourceID: i18n.TranslationSource{Name: "scooby-doo"},
+									GrafficoSourceID:      i18n.TranslationSource{Name: "test.graffico"},
 								},
 							}
 						}); err == nil {
@@ -386,17 +309,17 @@ var _ = Describe("Translator", Ordered, func() {
 						)
 					}
 				}()
-				if err := xi18n.Use(func(o *xi18n.UseOptions) {
+				if err := i18n.Use(func(o *i18n.UseOptions) {
 					o.Tag = language.AmericanEnglish
 					o.From.Path = l10nPath
-					o.From.Sources = xi18n.TranslationFiles{
-						xi18n.ExtendioSourceID: xi18n.TranslationSource{Name: "test"},
-						GrafficoSourceID:       xi18n.TranslationSource{Name: "test.graffico"},
+					o.From.Sources = i18n.TranslationFiles{
+						i18n.ExtendioSourceID: i18n.TranslationSource{Name: "test"},
+						GrafficoSourceID:      i18n.TranslationSource{Name: "test.graffico"},
 					}
 				}); err != nil {
 					Fail(err.Error())
 				}
-				_ = xi18n.Text(WrongSourceIDTemplData{})
+				_ = i18n.Text(WrongSourceIDTemplData{})
 				Fail("❌ expected panic due to invalid path: 'FOO-BAR'")
 			})
 		})
@@ -406,8 +329,8 @@ var _ = Describe("Translator", Ordered, func() {
 		Context("given: FailedToReadDirectoryContentsError", func() {
 			It("🧪 should: be identifiable via query function", func() {
 				reason := fmt.Errorf("file missing")
-				err := xi18n.NewFailedToReadDirectoryContentsError("/foo/bar/", reason)
-				result := xi18n.QueryFailedToReadDirectoryContentsError(err)
+				err := i18n.NewFailedToReadDirectoryContentsError("/foo/bar/", reason)
+				result := i18n.QueryFailedToReadDirectoryContentsError(err)
 				Expect(result).To(BeTrue())
 			})
 		})
@@ -415,68 +338,68 @@ var _ = Describe("Translator", Ordered, func() {
 		Context("given: NewFailedToResumeFromFileError", func() {
 			It("🧪 should: be identifiable via query function", func() {
 				reason := fmt.Errorf("file missing")
-				err := xi18n.NewFailedToResumeFromFileError("/foo/bar/resume.json", reason)
-				result := xi18n.QueryFailedToResumeFromFileError(err)
+				err := i18n.NewFailedToResumeFromFileError("/foo/bar/resume.json", reason)
+				result := i18n.QueryFailedToResumeFromFileError(err)
 				Expect(result).To(BeTrue())
 			})
 		})
 
 		Context("given: InvalidConfigEntryError", func() {
 			It("🧪 should: be identifiable via query function", func() {
-				err := xi18n.NewInvalidConfigEntryError("foo", "Store/Logging/Path")
-				result := xi18n.QueryInvalidConfigEntryError(err)
+				err := i18n.NewInvalidConfigEntryError("foo", "Store/Logging/Path")
+				result := i18n.QueryInvalidConfigEntryError(err)
 				Expect(result).To(BeTrue())
 			})
 		})
 
 		Context("given: InvalidResumeStrategyError", func() {
 			It("🧪 should: be identifiable via query function", func() {
-				err := xi18n.NewInvalidResumeStrategyError("foo")
-				result := xi18n.QueryInvalidResumeStrategyError(err)
+				err := i18n.NewInvalidResumeStrategyError("foo")
+				result := i18n.QueryInvalidResumeStrategyError(err)
 				Expect(result).To(BeTrue())
 			})
 		})
 
 		Context("given: MissingCallbackError", func() {
 			It("🧪 should: be identifiable via query function", func() {
-				err := xi18n.NewMissingCallbackError()
-				result := xi18n.QueryMissingCallbackError(err)
+				err := i18n.NewMissingCallbackError()
+				result := i18n.QueryMissingCallbackError(err)
 				Expect(result).To(BeTrue())
 			})
 		})
 
 		Context("given: MissingCustomFilterDefinitionError", func() {
 			It("🧪 should: be identifiable via query function", func() {
-				err := xi18n.NewMissingCustomFilterDefinitionError(
+				err := i18n.NewMissingCustomFilterDefinitionError(
 					"Options/Store/FilterDefs/Node/Custom",
 				)
-				result := xi18n.QueryMissingCustomFilterDefinitionError(err)
+				result := i18n.QueryMissingCustomFilterDefinitionError(err)
 				Expect(result).To(BeTrue())
 			})
 		})
 
 		Context("given: NotADirectoryError", func() {
 			It("🧪 should: be identifiable via query function", func() {
-				err := xi18n.NewNotADirectoryError("/foo/bar")
-				result := xi18n.QueryNotADirectoryError(err)
+				err := i18n.NewNotADirectoryError("/foo/bar")
+				result := i18n.QueryNotADirectoryError(err)
 				Expect(result).To(BeTrue())
 			})
 		})
 
 		Context("given: SortFnFailedError", func() {
 			It("🧪 should: be identifiable via query function", func() {
-				err := xi18n.NewSortFnFailedError()
-				result := xi18n.QuerySortFnFailedError(err)
+				err := i18n.NewSortFnFailedError()
+				result := i18n.QuerySortFnFailedError(err)
 				Expect(result).To(BeTrue())
 			})
 		})
 
 		Context("given: UnknownMarshalFormatError", func() {
 			It("🧪 should: be identifiable via query function", func() {
-				err := xi18n.NewUnknownMarshalFormatError(
+				err := i18n.NewUnknownMarshalFormatError(
 					"Options/Persist/Format", "jpg",
 				)
-				result := xi18n.QueryUnknownMarshalFormatError(err)
+				result := i18n.QueryUnknownMarshalFormatError(err)
 				Expect(result).To(BeTrue())
 			})
 		})
