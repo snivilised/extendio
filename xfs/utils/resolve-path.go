fd81839c124fd8008b9557da12e6f321895b4a30
plastikfan <plastikfan@outlook.com>
2023-08-19 08:57:37 +0100

    ref(utils): minor tweaks to resolvepath (#294)

--- xfs/utils/resolve-path.go
+++ xfs/utils/resolve-path.go
@@ -3,6 +3,8 @@ package utils
 import (
 	"os"
 	"path/filepath"
+
+	"github.com/samber/lo"
 )
 
 // AbsFunc signature of function used to obtain the absolute representation of
@@ -16,19 +18,19 @@ func (f AbsFunc) Abs(path string) (string, error) {
 }
 
 // HomeUserFunc signature of function used to obtain the user's home directory.
-type HomeUserFunc func() string
+type HomeUserFunc func() (string, error)
 
 // Home function invoker, allows a function to be used in place where
 // an instance of an interface would be expected.
-func (f HomeUserFunc) Home() string {
+func (f HomeUserFunc) Home() (string, error) {
 	return f()
 }
 
-// ResolveOverrides, used to override the internal functions used
+// ResolveMocks, used to override the internal functions used
 // to resolve the home path (os.UserHomeDir) and the abs path
-// (filepath.Abs). In normal usage, these do not need to be overridden,
+// (filepath.Abs). In normal usage, these do not need to be provided,
 // just used for testing purposes.
-type ResolveOverrides struct {
+type ResolveMocks struct {
 	HomeFunc HomeUserFunc
 	AbsFunc  AbsFunc
 }
@@ -36,31 +38,41 @@ type ResolveOverrides struct {
 // ResolvePath performs 2 forms of path resolution. The first is resolving a
 // home path reference, via the ~ character; ~ is replaced by the user's
 // home path. The second resolves ./ or ../ relative path. The overrides
-// does not need to be provided
-func ResolvePath(path string, overrides ...ResolveOverrides) string {
+// do not need to be provided.
+func ResolvePath(path string, mocks ...ResolveMocks) string {
 	result := path
 
-	if len(overrides) > 0 {
-		override := overrides[0]
-		if result[0] == '~' {
-			result = filepath.Join(override.HomeFunc(), result[1:])
-		} else {
-			a, err := override.AbsFunc(result)
-
-			if err == nil {
-				result = a
-			}
-		}
+	if len(mocks) > 0 {
+		m := mocks[0]
+		result = lo.TernaryF(result[0] == '~',
+			func() string {
+				if h, err := m.HomeFunc(); err == nil {
+					return filepath.Join(h, result[1:])
+				}
+				return path
+			},
+			func() string {
+				if a, err := m.AbsFunc(result); err == nil {
+					return a
+				}
+				return path
+			},
+		)
 	} else {
-		if result[0] == '~' {
-			if h, err := os.UserHomeDir(); err == nil {
-				result = filepath.Join(h, result[1:])
-			}
-		} else {
-			if a, err := filepath.Abs(result); err == nil {
-				result = a
-			}
-		}
+		result = lo.TernaryF(result[0] == '~',
+			func() string {
+				if h, err := os.UserHomeDir(); err == nil {
+					return filepath.Join(h, result[1:])
+				}
+				return path
+			},
+			func() string {
+				if a, err := filepath.Abs(result); err == nil {
+					return a
+				}
+				return path
+			},
+		)
 	}
 
 	return result
