4541bc35bcbb75c62f048199b6d60d1631fc9ae3
plastikfan <plastikfan@outlook.com>
2024-01-09 22:21:06 +0000

    feat(utils): add ensure-path-at (#403)

---
+++ xfs/utils/ensure-path-at_test.go
@@ -0,0 +1,80 @@
+package utils_test
+
+import (
+	"fmt"
+	"path/filepath"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/snivilised/extendio/internal/helpers"
+	"github.com/snivilised/extendio/xfs/matchers"
+	"github.com/snivilised/extendio/xfs/storage"
+	"github.com/snivilised/extendio/xfs/utils"
+)
+
+type ensureTE struct {
+	given     string
+	should    string
+	relative  string
+	expected  string
+	directory bool
+}
+
+const perm = 0o766
+
+var _ = Describe("EnsurePathAt", Ordered, func() {
+	var (
+		vfs   storage.VirtualFS
+		mocks *utils.ResolveMocks
+	)
+
+	BeforeEach(func() {
+		mocks = &utils.ResolveMocks{
+			HomeFunc: func() (string, error) {
+				return filepath.Join(string(filepath.Separator), "home", "prodigy"), nil
+			},
+			AbsFunc: func(_ string) (string, error) {
+				panic("not required for these tests")
+			},
+		}
+
+		vfs = storage.UseMemFS()
+	})
+
+	DescribeTable("with vfs",
+		func(entry *ensureTE) {
+			home, _ := mocks.HomeFunc()
+			location := filepath.Join(home, entry.relative)
+			if entry.directory {
+				location += string(filepath.Separator)
+			}
+
+			actual, err := utils.EnsurePathAt(location, "default-test.log", perm, vfs)
+			directory, _ := filepath.Split(actual)
+			expected := helpers.Path(home, entry.expected)
+
+			Expect(err).Error().To(BeNil())
+			Expect(actual).To(Equal(expected))
+			Expect(matchers.AsDirectory(directory)).To(matchers.ExistInFS(vfs))
+		},
+		func(entry *ensureTE) string {
+			return fmt.Sprintf("🧪 ===> given: '%v', should: '%v'", entry.given, entry.should)
+		},
+
+		Entry(nil, &ensureTE{
+			given:    "path with file",
+			should:   "create parent directory and return specified file path",
+			relative: "logs/test.log",
+			expected: "logs/test.log",
+		}),
+
+		Entry(nil, &ensureTE{
+			given:     "path with file",
+			should:    "create parent directory and return default file path",
+			relative:  "logs/",
+			directory: true,
+			expected:  "logs/default-test.log",
+		}),
+	)
+})
